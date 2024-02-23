89463474ce0b2baf32b4f093cdfde6bcbc4d335f
galievranis <galiev.ranisque@gmail.com>
2024-02-23 22:41:09 +0600

    feat: fix ssr to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://galievranis.github.io',
-  adapter: vercel()
+    site: 'https://galievranis.github.io'
 });
