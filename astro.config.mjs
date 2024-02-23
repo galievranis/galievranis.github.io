6cd3626e148564568345d683113c4659a8200c4d
galievranis <galiev.ranisque@gmail.com>
2024-02-23 22:29:25 +0600

    feat: add ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site: 'https://galievranis.github.io'
+  output: 'server',
+  site: 'https://galievranis.github.io',
+  adapter: vercel()
 });
